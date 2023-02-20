996a47c63859ddb7567b4b9b65b1cb373544911c
Nick Fed <fna.21@uni-dubna.ru>
2023-02-20 14:51:46 +0900

    regex

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ nm = nmap.PortScanner()
 #scan_range = nm.scan(hosts = "2400:2650:78c5:1200:608b:ea8f:a3f:aeda",
 #                     arguments="-6 -sV")
 
-scan_range = nm.scan(hosts="192.168.3.11",
-                     arguments="-sV -O")
+#scan_range = nm.scan(hosts="192.168.3.11",
+#                     arguments="-sV -O")
+
 #print(scan_range['scan'])
 # --script vulscan/vulscan.nse --script-args vulscandb=allitems.csv
 
@@ -21,17 +22,18 @@ scan_range = nm.scan(hosts="192.168.3.11",
 scan_range = nm.scan(hosts = "192.168.3.12",
                      arguments="-sV --script vulscan/vulscan.nse --script-args vulscandb=allitems.csv")
 
-scan_result = scan_range['scan']
+#scan_result = scan_range['scan']
 
 
-
-#jsondata = json.dumps(scan_range['scan'], indent = 10) 
-#print(jsondata)
+jsondata = json.dumps(scan_range['scan'], indent = 10) 
+print(jsondata)
 
 #df = pd.read_csv(nm.csv())
-#df = pd.read_json(jsondata)
-#df.to_csv("scan.csv")
-#df.to_json("scan.json")
+
+df = pd.read_json(jsondata)
+
+df.to_csv("scan.csv")
+df.to_json("scan.json")
 
 #https://www.cve.org/CVERecord?id=
 #https://nvd.nist.gov/vuln/detail/CVE-
