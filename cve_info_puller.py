af97173ec4b98e7662b262063b18b1d5fd1f3d5e
Nick Fed <fna.21@uni-dubna.ru>
2023-02-20 19:47:25 +0900

    working cve fetch from 1 source

--- cve_info_puller.py
+++ cve_info_puller.py
@@ -2,12 +2,32 @@
 
 import sys
 import re
+import requests
 from bs4 import BeautifulSoup
 
-cve = sys.argv
+def getHTML(url):
+    response = requests.get(url)
+    return response.text
 
-if (type(cve) != type(str) or re.fullmatch('^CVE-\d{4}-\d{4}$', cve)):
-    print(-1)
+if (len(sys.argv) != 2):
+    sys.exit("Incorrect input")
 
-#https://www.cve.org/CVERecord?id=
-#https://nvd.nist.gov/vuln/detail/CVE-
+cve = sys.argv[1]
+
+if (not re.fullmatch('^CVE-\d{4}-\d{4}$', cve)):
+    sys.exit("Incorrect input")
+
+print(cve)
+
+source_1 = 'https://www.cve.org/CVERecord?id='
+source_2 = 'https://nvd.nist.gov/vuln/detail/'
+
+html = getHTML(source_2 + cve)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+vuln_desc = soup.find('p', attrs={'data-testid': 'vuln-description'}).getText()
+cvss_score = soup.find('a', attrs={'data-testid': "vuln-cvss3-panel-score"}).getText()
+
+print("Description: ", vuln_desc)
+print("CVSS score: ", cvss_score)
