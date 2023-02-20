f954ce519a8ba960b284a9827e7b1759603722fd
Nick Fed <fna.21@uni-dubna.ru>
2023-02-20 23:48:33 +0900

    JSON output

--- cve_info_fetcher.py
+++ cve_info_fetcher.py
@@ -3,6 +3,7 @@
 import sys
 import re
 import requests
+import json
 from bs4 import BeautifulSoup
 
 def getHTML(url):
@@ -10,12 +11,12 @@ def getHTML(url):
     return response.text
 
 if (len(sys.argv) != 2):
-    sys.exit("Incorrect input")
+    sys.exit("Incorrect amount of input")
 
 cve = sys.argv[1]
 
 if (not re.fullmatch('^CVE-\d{4}-\d{4}$', cve)):
-    sys.exit("Incorrect input")
+    sys.exit("Incorrect input format")
 
 print(cve)
 
@@ -27,7 +28,16 @@ html = getHTML(source_2 + cve)
 soup = BeautifulSoup(html, 'html.parser')
 
 vuln_desc = soup.find('p', attrs={'data-testid': 'vuln-description'}).getText()
-cvss_score = soup.find('a', attrs={'data-testid': "vuln-cvss3-panel-score"}).getText()
+try:
+    cvss_score = soup.find('a', attrs={'data-testid': "vuln-cvss3-panel-score"}).getText()
+except:
+    cvss_score = "N/A"
 
-print("Description: ", vuln_desc)
-print("CVSS score: ", cvss_score)
+#print("Description: ", vuln_desc)
+#print("CVSS score: ", cvss_score)
+
+res = dict({"Description": vuln_desc, "CVSS score": cvss_score})
+
+sys.stdout.write(str(json.dumps(res, indent = 4) ))
+sys.stdout.flush()
+sys.exit(0)
